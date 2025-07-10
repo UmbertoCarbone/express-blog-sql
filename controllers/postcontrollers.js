ee1403ea621a2643fc087fce8cd95f33e60a1f90
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-10 17:19:39 +0200

    add milestone 4

--- controllers/postcontrollers.js
+++ controllers/postcontrollers.js
@@ -7,7 +7,7 @@ function index(req, res) {
 
   connection.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
-    
+
     res.json(results);
   });
 
@@ -22,15 +22,16 @@ function index(req, res) {
 }
 
 function show(req, res) {
-// recuperiamo l'id dall' URL
-const id = req.params.id
-const sql = 'SELECT * FROM posts WHERE id = ?';
-connection.query(sql, [id], (err, results) => {
-if (err) return res.status(500).json({ error: 'Database query failed' });
-if (results.length === 0) return res.status(404).json({ error: 'Posts not found' });
-res.json(results[0]);
-console.log(results)
-});
+  // recuperiamo l'id dall' URL
+  const id = req.params.id;
+  const sql = "SELECT * FROM posts WHERE id = ?";
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (results.length === 0)
+      return res.status(404).json({ error: "Posts not found" });
+    res.json(results[0]);
+    console.log(results);
+  });
 }
 
 /* function show(req, res) {
@@ -47,6 +48,16 @@ console.log(results)
   res.json(post);
   console.log(post);
 } */
+
+function destroy(req, res) {
+  // recuperiamo l'id dall' URL
+  const id = req.params;
+  //Eliminiamo la pizza dal menu
+  connection.query("DELETE FROM posts WHERE id = ?", [id], (err) => {
+    if (err) return res.status(500).json({ error: "Failed to delete pizza" });
+    res.sendStatus(204);
+  });
+}
 
 function post(req, res) {
   const newPostId = posts[posts.length - 1].id + 1;
@@ -81,23 +92,6 @@ function update(req, res) {
   (post.image = req.body.image), (post.tags = req.body.tags);
   console.log(post);
   res.json(post);
-}
-
-function destroy(req, res) {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res.status(404);
-
-    return res.json({
-      status: 404,
-      error: "Not found",
-      message: "Post non trovato",
-    });
-  }
-  posts.splice(posts.indexOf(post), 1);
-  res.sendStatus(204);
-  console.log(posts);
 }
 
 module.exports = {
