e73bce22715c7293785e482f5b5da80c063a9e56
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-11 17:27:55 +0200

    add update and post

--- controllers/postcontrollers.js
+++ controllers/postcontrollers.js
@@ -1,5 +1,6 @@
 /* const posts = require("../data/posts"); */
 
+const { parse } = require("dotenv");
 const connection = require("../data/db");
 
 function index(req, res) {
@@ -24,7 +25,7 @@ function index(req, res) {
 function show(req, res) {
   // recuperiamo l'id dall' URL
   const id = req.params.id;
-  
+
   const sql = "SELECT * FROM posts WHERE id = ?";
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
@@ -35,50 +36,76 @@ function show(req, res) {
   });
 }
 
-/* function show(req, res) {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res.status(404);
-    return res.json({
-      status: 404,
-      error: "Not found",
-      message: "Post non trovato",
-    });
-  }
-  res.json(post);
-  console.log(post);
-} */
-
 function destroy(req, res) {
   // recuperiamo l'id dall' URL
   const id = req.params.id;
   console.log(id);
-  
+
   //Eliminiamo la pizza dal menu
   connection.query("DELETE FROM posts WHERE id = ?", [id], (err) => {
-    if (err) return res.status(500).json({ error: "Failed to delete pizza" });
+    if (err) return res.status(500).json({ error: "Posts not deleted" });
     res.sendStatus(204);
   });
 }
 
 function post(req, res) {
-  const newPostId = posts[posts.length - 1].id + 1;
-  //creiamo un nuovo oggetto post
-  const newPostObj = {
-    id: newPostId,
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-    tags: req.body.tags,
-  };
-  //aggiungiamo il nuovo post
-  posts.push(newPostObj);
-  //restituiamo lo status corretto e il post creato
-  res.status(201).json(newPostObj);
-  console.log(newPostObj);
+  const { title, content, image } = req.body;
+
+  const sql = "INSERT INTO posts (title, content, image) VALUES (? , ?, ?)";
+  connection.query(sql, [title, content, image], (err, results) => {
+    if (err) return res.status(500).json({ error: "failed create new post" });
+    res.status(201).json(results);
+  });
 }
+
 function update(req, res) {
+  const id = parseInt(req.params.id);
+  const { title, content, image } = req.body;
+
+  const sql = "UPDATE posts SET title = ?, content = ?, image = ? WHERE id = ?";
+  connection.query(sql, [title, content, image, id], (err, results) => {
+    if (err) return res.status(500).json({ error: "failed to update post" });
+    if (results.affectedRows === 0)
+      return res.status(404).json({ error: "Post not found" });
+    // res.json({ message: "Post updated successfully",  result: results  });
+  });
+
+  const sqlSinglePost = "SELECT * FROM posts WHERE id = ?";
+  connection.query(sqlSinglePost, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+
+    if (results.length === 0)
+      return res.status(404).json({ error: "Posts not found" });
+
+    // res.json(results[0]);
+    res.json({ message: "Post updated successfully", result: results[0] });
+  });
+}
+
+module.exports = {
+  index,
+  show,
+  post,
+  update,
+  destroy,
+};
+
+// const newPostId = posts[posts.length - 1].id + 1;
+// //creiamo un nuovo oggetto post
+// const newPostObj = {
+//   id: newPostId,
+//   title: req.body.title,
+//   content: req.body.content,
+//   image: req.body.image,
+//   tags: req.body.tags,
+// };
+// //aggiungiamo il nuovo post
+// posts.push(newPostObj);
+//restituiamo lo status corretto e il post creato
+// res.status(201).json(newPostObj);
+/* console.log(newPostObj); */
+
+/* function update(req, res) {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
@@ -95,12 +122,19 @@ function update(req, res) {
   (post.image = req.body.image), (post.tags = req.body.tags);
   console.log(post);
   res.json(post);
-}
+} */
 
-module.exports = {
-  index,
-  show,
-  post,
-  update,
-  destroy,
-};
+/* function show(req, res) {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    res.status(404);
+    return res.json({
+      status: 404,
+      error: "Not found",
+      message: "Post non trovato",
+    });
+  }
+  res.json(post);
+  console.log(post);
+} */
